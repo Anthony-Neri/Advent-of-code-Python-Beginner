42e13b3d2078eb406e09b8a1c11d87becca8d0d5
Anthony <anthony.ero25@protonmail.com>
2026-01-12 18:54:14 +0100

    Create 2025-1.py

---
+++ 2025-1.py
@@ -0,0 +1,27 @@
+def extract_data_file(file):
+    # Lit toutes les lignes du fichier
+    with open(file, 'r', encoding='utf-8') as f:
+        return f.read().splitlines()
+
+file = "data/2025_1.txt"
+
+jeu_de_donne = extract_data_file(file)
+
+position = 50
+nbrPosition0 = 0
+for line in jeu_de_donne :
+    number = int(line[1:])
+    if line[0] == 'L':
+        position-= number
+    elif line[0] == 'R':
+        position+= number
+    
+    while position > 99 :
+        position-=100
+    while position < 0 :
+        position+=100
+    
+    if position == 0:
+        nbrPosition0+=1
+    
+print(nbrPosition0)
